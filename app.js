a658cac063de5136da64ec29d8d115225863c274
carneiroedu <116359172+carneiroedu@users.noreply.github.com>
2024-09-06 10:58:07 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa")
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+    if(!campoPesquisa) {
+        section.innerHTML = "<p> Busca alguma coisa pô... </p>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase() 
+
+let resultados = ""
+let titulo = ""
+let descricao = "" 
+let tags = ""
+
+for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLowerCase()
+    if(titulo.includes(campoPesquisa ) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        resultados += `
+        <div class="item-resultado">
+            <h2>
+                <a href="#" target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="descrição-meta">${dado.descricao}</p>
+            <a href=${dado.link}target="_blank">Sobre Fantasmas do Cyberverso</a>
+        </div>` 
+    }
+    
+}
+
+if(!resultados) {
+    resultados = "<p>Nada  foi encontrado</p>"
+}
+
+section.innerHTML = resultados 
+}
+
+
+ 
---
+++ dados.js
@@ -0,0 +1,44 @@
+let dados = [
+    {
+        titulo : "Victor Gabriel 'Phantom' Rodrigues",
+        descricao : "Victor é um Cyberunner, um hacker que utiliza a realidade virtual para atacar sistemas de grandes corporações. Após perder o que tinha de mais valioso, ele adentra em uma jornada de justiça pessoal, visando eliminar as grandes e corruptas Corporações Alteanas.",
+        link: "https://fantasmasdocyberversoteste.blogspot.com/2024/09/victor-gabriel-phantom-rodrigues-de.html",
+        tags: "hacker protagonista brasileiro neurodriver"
+    }, 
+    {
+        titulo : "Sofia 'Rosa' Miralles",
+        descricao : "Uma ladra de carros e uma astuta piloto pelas ruas neon movimentadas de Pulse City, Rosa vive em uma corrida eterna contra o sistema, buscando a tão sonhada paz. No entanto, para isso, sacrifícios são exigidos.",
+        link: "https://fantasmasdocyberversoteste.blogspot.com/2024/09/victor-gabriel-phantom-rodrigues-de.html", 
+        tags: "moto ladra protagonista"
+    },
+    {
+        titulo : "Marshall J. 'Wrecker' Jackson",
+        descricao : "O líder dos Uprisers, Wrecker viveu o caos das Cidades Baixas durante toda sua infância. Acreditando em um futuro de liberdade para sua família, Wrecker luta por um mundo melhor, liderando sua gangue contra as forças armadas da S.P.E.A.R",
+        link: "https://fantasmasdocyberversoteste.blogspot.com/2024/09/victor-gabriel-phantom-rodrigues-de.html",
+        tags: "50 cent herói soco manoplas gauntlet"
+    },
+    {
+        titulo : "Kenji",
+        descricao : "Um ronin cibernético, com o corpo quase totalmente modificado pela Yakuza japonesa para tornar-se uma arma viva, Kenji é assombrado por seu passado e busca seu verdadeiro propósito no mundo.",
+        link: "https://fantasmasdocyberversoteste.blogspot.com/2024/09/victor-gabriel-phantom-rodrigues-de.html",
+        tags: "ninja ciborgue relâmpago raijin takeshi"
+    }, 
+    {
+        titulo : "B.L.I.T.Z",
+        descricao : "Criado por Wrecker com uma Síntese de Vida - um dispositivo que atribui personalidade a uma IA -, Blitz, um robô, tem a missão de proteger seus companheiros de equipe, e a aprender a lidar com sua própria humanidade (Ou a sua ausência).",
+        link: "https://fantasmasdocyberversoteste.blogspot.com/2024/09/victor-gabriel-phantom-rodrigues-de.html",
+        tags: "robô gear gears phoenix chimera dragon vivo oasis"
+    },
+    {
+        titulo : "Nova",
+        descricao : "Nova é uma Sintética, um ser criado industrialmente para imitar um ser-humano, mas com a pele cinzenta e olhos escuros. Em um mundo feito para odiá-la apenas por ser quem é, Nova enfrenta seus criadores e busca igualdade para os Artificiais.",
+        link: "https://fantasmasdocyberversoteste.blogspot.com/2024/09/victor-gabriel-phantom-rodrigues-de.html",
+        tags: "sintético industrial artificial amor"
+    },
+    {
+        titulo : "Ellen Margot Navarro",
+        descricao : "A esposa de Wrecker e uma cientista brilhante, Ellen busca render-se por seu passado em uma das mais poderosas corporações Alteanas.",
+        link: "https://fantasmasdocyberversoteste.blogspot.com/2024/09/victor-gabriel-phantom-rodrigues-de.html",
+        tags: "medica soro heratech"
+    },
+]
