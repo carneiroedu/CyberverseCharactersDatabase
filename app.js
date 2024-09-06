75a12e51f07a5d9354b72be571c4a4604546d25d
carneiroedu <116359172+carneiroedu@users.noreply.github.com>
2024-09-06 12:57:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ for (let dado of dados) {
         resultados += `
         <div class="item-resultado">
             <h2>
-                <a href="#" target="_blank">${dado.titulo}</a>
+                <a href="#">${dado.titulo}</a>
             </h2>
             <p class="descrição-meta">${dado.descricao}</p>
             <a href=${dado.link}target="_blank">Sobre Fantasmas do Cyberverso</a>
